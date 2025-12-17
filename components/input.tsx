87dc9800e80e7bbe04c3cc53096e96d7cee3e042
rathore  smit <rathodesmit2177@.com>
2025-12-17 12:51:04 +0530

    Refactor InputCom component; streamline state management and improve layout

--- components/input.tsx
+++ components/input.tsx
@@ -3,17 +3,37 @@
 import { useState } from "react";
 
 export const InputCom = () => {
+  const [text, setText] = useState('');
+  const [data, setData] = useState<string[]>([]);
 
-    const [text, setText] = useState('');
+  const handleClick = () => {
+    setData((prev) => [...prev, text]);
+    setText('');
+  };
 
-
-const handleClick = () => {
-    console.log(text);
-}
   return (
     <div>
-        <input type ="text" placeholder='Add your todo' value={text} onChange={(e) => setText(e.target.value)} className='border-2 border-gray-300 p-2 rounded-md w-full'/>
-        <button onClick={handleClick} className="mt-2 bg-blue-500 text-white p-2 rounded-md w-full">Add Todo</button>
+      <input
+        type="text"
+        placeholder="Add your todo"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        className="border-2 border-gray-300 p-2 rounded-md w-full"
+      />
+
+      <button
+        onClick={handleClick}
+        className="mt-2 bg-blue-500 text-white p-2 rounded-md w-full"
+      >
+        Add Todo
+      </button>
+      <ul className="mt-4 border-t-2 border-gray-300 pt-2">
+        {data.map((item, index) => (
+          <li key={index} className=" py-1">
+            {item}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
